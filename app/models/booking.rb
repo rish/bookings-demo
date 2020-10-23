d1c58b07a8c8f401161c8f2f3e4d6ce4509efbe6
Rish <rish@users.noreply.github.com>
2020-10-23 10:55:49 +0100

    Add bookings controller and filterable concern

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,6 @@
 class Booking < ApplicationRecord
+  include Filterable
+  scope :filter_by_space_id, -> (space_id) { where space_id: space_id}
+
   belongs_to :space
 end
---
+++ app/views/bookings/_booking.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! booking, :id, :created_at, :updated_at, :name, :description, :start_date, :end_date, :space_id
