62fc45b7983de69f7e9705843c390fc0beea12b7
Rish <rish@users.noreply.github.com>
2020-10-23 12:12:13 +0100

    Update db seeds files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Space.create!([
+  {name: "Space 1", building_id: 1},
+  {name: "Space 2", building_id: 1},
+  {name: "Space 3", building_id: 1},
+  {name: "Space 4", building_id: 1},
+  {name: "Space 5", building_id: 1}
+])
+Building.create!([
+  {name: "Building 1"}
+])
+Booking.create!([
+  {name: "test 1", description: "just running a test", start_date: "2020-10-23", end_date: "2020-10-24", space_id: 5},
+  {name: "Test 2", description: "Another test", start_date: "2020-10-23", end_date: "2020-10-24", space_id: 5}
+])
